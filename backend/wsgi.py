2de0315f6303ebe5c8e704c9ae972691bf97e5f8
Devanuj Rijal <rijaldevanuj@gmail.com>
2025-06-28 11:29:25 +0530

    wsgi.py file created to run the wsgi server

---
+++ backend/wsgi.py
@@ -0,0 +1,7 @@
+# wsgi.py
+from flask_Server import app
+
+if __name__ == "__main__":
+    app.run()
+# This file is used to run the Flask application with a WSGI server.
+# It imports the Flask app from flask_Server.py and runs it.                                                
